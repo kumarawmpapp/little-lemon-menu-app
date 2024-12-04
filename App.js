333fc71a4cdb42671787bcfcbec99e777a659328
Pradeep Kumara <kumarawmpapp@gmail.com>
2024-12-04 20:46:36 +0530

    fetch data
    implement fetch data method

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useMemo } from 'react';
+import { useEffect, useState, useCallback, useMemo } from "react";
 import {
   Text,
   View,
@@ -7,21 +7,21 @@ import {
   SafeAreaView,
   StatusBar,
   Alert,
-} from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import debounce from 'lodash.debounce';
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+import debounce from "lodash.debounce";
 import {
   createTable,
   getMenuItems,
   saveMenuItems,
   filterByQueryAndCategories,
-} from './database';
-import Filters from './components/Filters';
-import { getSectionListData, useUpdateEffect } from './utils';
+} from "./database";
+import Filters from "./components/Filters";
+import { getSectionListData, useUpdateEffect } from "./utils";
 
 const API_URL =
-  'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json';
-const sections = ['Appetizers', 'Salads', 'Beverages'];
+  "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json";
+const sections = ["Appetizers", "Salads", "Beverages"];
 
 const Item = ({ title, price }) => (
   <View style={styles.item}>
@@ -32,20 +32,33 @@ const Item = ({ title, price }) => (
 
 export default function App() {
   const [data, setData] = useState([]);
-  const [searchBarText, setSearchBarText] = useState('');
-  const [query, setQuery] = useState('');
+  const [searchBarText, setSearchBarText] = useState("");
+  const [query, setQuery] = useState("");
   const [filterSelections, setFilterSelections] = useState(
     sections.map(() => false)
   );
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     // 1. Implement this function
-    
+
     // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
     // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
     // So the server response should be slighly transformed in this function (hint: map function) to flatten out each menu item in the array,
-    return [];
-  }
+    try {
+      const response = await fetch(
+        API_URL
+      );
+      const json = await response.json();
+      return json.menu.map(item => {
+        let modifiedItem = item;
+        modifiedItem.category = item.category.title;
+        return modifiedItem;
+      });
+    } catch (error) {
+      console.error(error);
+      return [];
+    } 
+  };
 
   useEffect(() => {
     (async () => {
@@ -118,7 +131,7 @@ export default function App() {
         value={searchBarText}
         style={styles.searchBar}
         iconColor="white"
-        inputStyle={{ color: 'white' }}
+        inputStyle={{ color: "white" }}
         elevation={0}
       />
       <Filters
@@ -145,31 +158,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
-    backgroundColor: '#495E57',
+    backgroundColor: "#495E57",
   },
   sectionList: {
     paddingHorizontal: 16,
   },
   searchBar: {
     marginBottom: 24,
-    backgroundColor: '#495E57',
+    backgroundColor: "#495E57",
     shadowRadius: 0,
     shadowOpacity: 0,
   },
   item: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 16,
   },
   header: {
     fontSize: 24,
     paddingVertical: 8,
-    color: '#FBDABB',
-    backgroundColor: '#495E57',
+    color: "#FBDABB",
+    backgroundColor: "#495E57",
   },
   title: {
     fontSize: 20,
-    color: 'white',
+    color: "white",
   },
 });
